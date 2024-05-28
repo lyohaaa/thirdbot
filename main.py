dbc4124aa4e7815fb6657d73e22992a3b195e46c
Cybersport <Cybersport@26kadr.internal>
2024-05-28 14:56:40 +0300

    db

--- main.py
+++ main.py
@@ -2,13 +2,21 @@ from aiogram import Bot, Dispatcher, types, executor
 from config import TELEGRAM_TOKEN
 from keyboard.keyboards import get_keyboard_1, get_keyboard_2
 from keyboard.key_inline import get_keyboard_inline, get_keyboard_inline_2
+from database.database import initialize_db, add_user, get_user
 
 bot = Bot(token = TELEGRAM_TOKEN)
 dp = Dispatcher(bot)
 
+initialize_db()
+
 @dp.message_handler(commands= 'start')
 async def start(message: types.Message):
-    await message.reply('Привет, я второй бот', reply_markup=get_keyboard_1())
+    user = get_user(message.from_user.id)
+    if user is None:
+        add_user(message.from_user.id, message.from_user.username, message.from_user.first_name, message.from_user.last_name)
+        await message.reply('Привет, я третий бот', reply_markup=get_keyboard_1())
+    else:
+        await message.reply('Привет, я третий бот', reply_markup=get_keyboard_1())
 
 @dp.message_handler(lambda message: message.text == 'Отправь фото кота')
 async def button_1_click(message: types.Message):
